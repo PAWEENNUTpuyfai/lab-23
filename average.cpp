b9f9eef906980a5b7e59deb88b6d88b10c7f9b2f
PAWEENNUTpuyfai <PAWEENNUTpuyfai>
2023-03-03 15:17:52 +0700

    lap23-1c

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
-
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main(int argc,char* argv[]){
+    int sum=0,a;
+    if(argc == 1){
+        cout << "Please input numbers to find average."<< endl;
+    }else{
+        for(int i =1;i < argc;i++){
+            a = atoi(argv[i]);
+            sum += a;
+        }
+        cout << "---------------------------------" << endl ;
+        cout << "Average of " << argc-1 <<" numbers = " << sum/(argc-1) << endl;
+        cout << "---------------------------------"  ;
+    }
+   
+}
